2cfa1afcf6ee8e2baed9817b4bf1514743e5a34f
ndjerrou <modernejd@gmail.com>
2020-05-28 13:29:13 +0200

    Updates

---
+++ precache-manifest.d0175bb0206159b8800cfc0a6b9670e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f9823db0d66ffe2d8acb57fcb25f4f5",
+    "url": "/pick-images/index.html"
+  },
+  {
+    "revision": "d5c88796f07a22c35700",
+    "url": "/pick-images/static/css/2.c3343ac3.chunk.css"
+  },
+  {
+    "revision": "e633d003f4a1f61aeae0",
+    "url": "/pick-images/static/css/main.310e5213.chunk.css"
+  },
+  {
+    "revision": "d5c88796f07a22c35700",
+    "url": "/pick-images/static/js/2.61976a40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pick-images/static/js/2.61976a40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e633d003f4a1f61aeae0",
+    "url": "/pick-images/static/js/main.8461a20a.chunk.js"
+  },
+  {
+    "revision": "57808a6655dea8a0b9da",
+    "url": "/pick-images/static/js/runtime-main.e47113f8.js"
+  }
+]);
